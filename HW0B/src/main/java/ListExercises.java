f6ed0f9ebea6bda3c532ed5de826d0fbdcf39012
LSLCS <shenstonelee@gmail.com>
2023-03-08 12:04:30 -0600

    completed lab2setup

--- HW0B/src/main/java/ListExercises.java
+++ HW0B/src/main/java/ListExercises.java
@@ -3,47 +3,73 @@ import java.util.List;
 
 public class ListExercises {
 
-//    sum: This method takes a list List<Integer> L and returns the total sum of the elements in that list.
-//    If the list is empty, it method should return 0.
-    public Integer sum(List<Integer> L){
+    /**
+     * sum: This method takes a list List<Integer> L and
+     * returns the total sum of the elements in that list.
+     * If the list is empty, it method should return 0.
+     * @param L
+     * @return
+     */
+    public Integer sum(List<Integer> L) {
         Integer total = 0;
-        for(int i : L){ total += i; }
+        for (int i : L) {
+            total += i;
+        }
         return total;
     }
 
-//    evens: This method takes a list List<Integer> L and returns a new list containing the even numbers of the given list.
-//    If there are no even elements, it should return an empty list.
-    public List<Integer> evens(List<Integer> L){
+    //
+
+    /**
+     * evens: This method takes a list List<Integer> L and
+     * returns a new list containing the even numbers of the given list.
+     * If there are no even elements, it should return an empty list.
+     * @param L
+     * @return List<Integer>
+     */
+    public List<Integer> evens(List<Integer> L) {
         List<Integer> newLst = new ArrayList<>();
-        for(int i : L){
-            if(i%2 == 0) {
+        for (int i : L) {
+            if (i % 2 == 0) {
                 newLst.add(i);
             }
         }
         return newLst;
     }
 
-//    common: This method takes two lists List<Integer> L1, List<Integer> L2 and returns a new list containing the common item of the two given lists.
-//    If there are no common items, it should return an empty list.
-    public List<Integer> common(List<Integer> L1, List<Integer> L2){
+    /**
+     * common: This method takes two lists List<Integer> L1, List<Integer> L2 and
+     * returns a new list containing the common item of the two given lists.
+     * If there are no common items, it should return an empty list.
+     * @param L1
+     * @param L2
+     * @return
+     */
+    public List<Integer> common(List<Integer> L1, List<Integer> L2) {
         List<Integer> newLst = new ArrayList<>();
-        for(int i : L1){
-            for(int j : L2)
-            if(i == j) {
-                newLst.add(i);
+        for (int i : L1) {
+            for (int j : L2) {
+                if (i == j) {
+                    newLst.add(i);
+                }
             }
         }
         return newLst;
     }
 
-//    countOccurrencesOfC: This method takes a list and a character List<String> words, char c and
-//    returns the number of occurrences of the given character in a list of strings.
-//    If the character does not occur in any of the words, it should return 0.
-    public Integer countOccurrencesOfC(List<String> words, char c){
+    /**
+     * countOccurrencesOfC: This method takes a list and a character List<String> words, char c and
+     * returns the number of occurrences of the given character in a list of strings.
+     * If the character does not occur in any of the words, it should return 0.
+     * @param words
+     * @param c
+     * @return
+     */
+    public Integer countOccurrencesOfC(List<String> words, char c) {
         Integer occurrences = 0;
-        for (String s : words){
-            for(int i = 0; i< s.length(); i++){
-                if (s.charAt(i)==c){
+        for (String s : words) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == c) {
                     occurrences += 1;
                 }
             }
@@ -53,11 +79,11 @@ public class ListExercises {
 
     public static void main(String[] args) {
         List<Integer> lst = new ArrayList<>();
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             lst.add(i);
         }
         List<Integer> lst2 = new ArrayList<>();
-        for (int i = 2; i<7; i++){
+        for (int i = 2; i < 7; i++) {
             lst2.add(i);
         }
 
@@ -69,8 +95,8 @@ public class ListExercises {
         ListExercises obj = new ListExercises();
         System.out.println(obj.sum(lst));
         System.out.println(obj.evens(lst));
-        System.out.println(obj.common(lst,lst2));
-        System.out.println(obj.countOccurrencesOfC(words,'i'));
+        System.out.println(obj.common(lst, lst2));
+        System.out.println(obj.countOccurrencesOfC(words, 'i'));
 
     }
 }
--- HW0B/src/main/java/MapExercises.java
+++ HW0B/src/main/java/MapExercises.java
@@ -1,36 +1,45 @@
 import java.util.*;
 
 public class MapExercises {
-//    letterToNum: This method returns a map from every lower case letter to the number corresponding to that letter starting with ‘a’ is 1.
-    public Map<Character,Integer> letterToNum(){
-        Map<Character,Integer> map = new TreeMap<>();
-        for (int i = 0; i+'a'<='z';i++){
-            map.put((char)(i+'a'),i+1);
-        }
-//        Map<Character,Integer> map = new HashMap<>();
-//        for (int i = 0; i+'a'<='z';i++){
-//            map.put((char)(i+'a'),i+1);
-//        }
-//        map.put('A',100);
-//        map.put('D',200);
-        return map;
-    }
 
-//    squares: This method takes a list List<Integer> nums and returns a map from the integers in the list to their squares.
-//    If the given list is empty, it should return an empty map.
-    public Map<Integer,Integer> squares(List<Integer> nums){
-        Map<Integer,Integer> map = new TreeMap<>();
-        for (int i : nums){
-            map.put(i, i*i);
+    /**
+     * letterToNum: This method returns a map from every lower case letter to
+     * the number corresponding to that letter starting with ‘a’ is 1.
+     * @return
+     */
+    public Map<Character, Integer> letterToNum() {
+        Map<Character, Integer> map = new TreeMap<>();
+        for (int i = 0; i + 'a' <= 'z'; i++) {
+            map.put((char) (i + 'a'), i + 1);
         }
         return map;
     }
 
-//    countWords: This method takes a list List<String> words and returns a map of the counts of all words that appear in a list of words.
-//    If the given list is empty, it should return an empty map.
-    public Map<String,Integer> countWords(List<String> words){
-        Map<String,Integer> map = new TreeMap<>();
-        for (String i : words){
+    /**
+     * squares: This method takes a list List<Integer> nums and
+     * returns a map from the integers in the list to their squares.
+     * If the given list is empty, it should return an empty map.
+     * @param nums
+     * @return
+     */
+    public Map<Integer, Integer> squares(List<Integer> nums) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i : nums) {
+            map.put(i, i * i);
+        }
+        return map;
+    }
+
+    /**
+     * countWords: This method takes a list List<String> words and
+     * returns a map of the counts of all words that appear in a list of words.
+     * If the given list is empty, it should return an empty map.
+     * @param words
+     * @return
+     */
+    public Map<String, Integer> countWords(List<String> words) {
+        Map<String, Integer> map = new TreeMap<>();
+        for (String i : words) {
             map.put(i, i.length());
         }
         return map;
@@ -40,7 +49,7 @@ public class MapExercises {
     public static void main(String[] args) {
         MapExercises obj = new MapExercises();
         List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             nums.add(i);
         }
         List<String> words = new ArrayList<>();
