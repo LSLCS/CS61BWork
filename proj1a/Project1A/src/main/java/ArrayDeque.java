35a222b4d31bf098019659f9af960a9402f0a9fb
LSLCS <shenstonelee@gmail.com>
2023-05-01 22:38:31 -0500

    project 1A

---
+++ proj1a/Project1A/src/main/java/ArrayDeque.java
@@ -0,0 +1,133 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private int arrayLength;
+    private float arrayUsage;
+
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        arrayLength = 8;
+        nextFirst = 0;
+        nextLast = 1;
+        arrayUsage = 0;
+    }
+
+    private void resize(int newLength){
+        T[] newArray = (T[]) new Object[newLength];
+        for (int i = 0; i < size; i++) {
+            int arrayIndex;
+            arrayIndex = (i + nextFirst + 1) >= arrayLength ?
+                    (i + nextFirst + 1 - arrayLength) : (i + nextFirst + 1);
+            newArray[i] = items [arrayIndex];
+        }
+        arrayLength = newLength;
+        nextFirst = newLength - 1;
+        nextLast = size;
+        items = newArray;
+    }
+
+    private void checkArrayUsage() {
+        if (arrayLength >= 16) {
+            arrayUsage = size * 100 / arrayLength;
+            if (arrayUsage < 25) {
+                resize(arrayLength / 2);
+            }
+        }
+    }
+
+    public void addFirst(T item) {
+        if (size == arrayLength) {
+            resize(size * 2);
+        }
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1) < 0 ? (arrayLength + nextFirst - 1) : (nextFirst - 1);
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if (size == arrayLength) {
+            resize(size * 2);
+        }
+        items[nextLast] = item;
+        nextLast = (nextLast + 1) == arrayLength ? (nextLast + 1 - arrayLength) : (nextLast + 1);
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if (isEmpty()) {
+            return;
+        }
+        for (int i = 1; i <= size; i++) {
+            int printIndex;
+            printIndex = (i + nextFirst) >= arrayLength ? (i + nextFirst - arrayLength) : (i + nextFirst);
+            System.out.print(items[printIndex]);
+            if (i != size) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        int firstIndex;
+        firstIndex = (nextFirst + 1) >= arrayLength ? (nextFirst + 1 - arrayLength) : (nextFirst + 1);
+        size -= 1;
+        T firstItem = items[firstIndex];
+        items[firstIndex] = null;
+        nextFirst = firstIndex;
+
+        checkArrayUsage();
+        return firstItem;
+    }
+
+
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        int lastIndex;
+        lastIndex = (nextLast - 1) < 0 ? (arrayLength + nextLast - 1) : (nextLast - 1);
+        size -= 1;
+        T lastItem = items[lastIndex];
+        items[lastIndex] = null;
+        nextLast = lastIndex;
+
+        checkArrayUsage();
+        return lastItem;
+    }
+
+    public T get(int index) {
+        if (isEmpty() || index > size || index < 0) {
+            return null;
+        }
+        int getArrayIndex;
+        getArrayIndex = (nextFirst + index + 1) >= arrayLength ?
+                (nextFirst + index + 1 - arrayLength) : (nextFirst + index + 1);
+        return items[getArrayIndex];
+    }
+
+
+
+}
---
+++ proj1a/Project1A/src/main/java/LinkedListDeque.java
@@ -0,0 +1,111 @@
+public class LinkedListDeque<T> {
+    private class Alloc {
+        public T item;
+        public Alloc prev;
+        public Alloc next;
+
+        public Alloc(T item, Alloc prev, Alloc next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public Alloc() {
+        }
+    }
+
+    private Alloc sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Alloc();
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public void addFirst(T item) {
+        sentinel.next = new Alloc(item, sentinel, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        sentinel.prev = new Alloc(item, sentinel.prev, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if (isEmpty()) {
+            return;
+        }
+        Alloc toPrint = sentinel;
+        for (int i = 0; i < size - 1; i++) {
+            toPrint = toPrint.next;
+            System.out.print(toPrint.item + " ");
+        }
+        System.out.println(toPrint.next.item);
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        T first = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        return first;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T last = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -= 1;
+        return last;
+    }
+
+    public T get(int index) {
+        if (isEmpty() || index > size || index < 0) {
+            return null;
+        }
+        Alloc toGet = sentinel;
+        for (int i = 0; i < index; i++) {
+            toGet = toGet.next;
+        }
+        return toGet.next.item;
+    }
+
+    public T getRecursive(int index) {
+        if (isEmpty() || index > size || index < 0) {
+            return null;
+        }
+        return getAllocRecursive(index,sentinel.next).item;
+    }
+
+    private Alloc getAllocRecursive(int index, Alloc a) {
+        if (index == 0) {
+            return a;
+        }
+        return getAllocRecursive(index-1, a.next);
+    }
+
+}
